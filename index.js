28b7b6166676a67997189127ec7b45c3e264d067
Megha Saji <meghasaji2002@gmail.com>
2023-11-29 21:02:35 +0530

    tasty palette-backend

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+const jsonServer= require('json-server')
+
+ const recipeAppServer =jsonServer.create()
+
+const router = jsonServer.router('db.json')
+
+const middleware =jsonServer.defaults()
+
+recipeAppServer.use(middleware)
+recipeAppServer.use(router)
+
+const port = 4000 || process.env.PORT
+
+recipeAppServer.listen(port,()=>{
+    console.log(`recepiAppServer started at ${port} and ready to fetch request`);
+})
